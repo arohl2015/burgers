dcd25d19e92b5c789feefe678b04173103704897
A Rohl <april.rohl2015@gmail.com>
2020-05-05 22:12:58 -0700

    handlebars files/additional folders & files added

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,59 @@
+/ Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+      var id = $(this).data("id");
+      var nowDevoured = $(this).data("nowDevoured");
+
+      var newDevouredState = {
+        devoured: nowDevoured
+      }
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevouredState
+      }).then(
+        function() {
+          console.log("changed burger state");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#burgerName").val().trim(),
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("new burger created!");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    // $(".delete-cat").on("click", function(event) {
+    //   var id = $(this).data("id");
+  
+    //   // Send the DELETE request.
+    //   $.ajax("/api/cats/" + id, {
+    //     type: "DELETE"
+    //   }).then(
+    //     function() {
+    //       console.log("deleted cat", id);
+    //       // Reload the page to get the updated list
+    //       location.reload();
+    //     }
+    //   );
+    // });
+  });
